bca0dbc87171a38701a55e592b01b6dba385cb0b
mardan15 <arash.official15@gmail.com>
2016-02-04 16:37:17 +0330

    uped

--- plugins/LeaveGroup.lua
+++ plugins/LeaveGroup.lua
@@ -1,20 +1,20 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
+local bot_id = 177958856 -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
+    if matches[1] == 'leavegroup' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, 'Fuck Inviters!.', ok_cb, false)
+      chat_del_user("chat#id"..msg.to.id, 'user#id'..177958856, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
+    "^[!/](leavegroup)$",
     "^!!tgservice (.+)$",
   },
   run = run
