2e67f804cef05abe07973bd69651cc86585195a1
mardan15 <arash.official15@gmail.com>
2016-02-04 15:15:22 +0330

    Update and rename stats.lua to STATS.lua

--- plugins/STATS.lua
+++ plugins/STATS.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'tauchtg' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -120,9 +120,9 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
-      if not is_admin(msg) then
-        return "For admins only !"
+    if matches[2] == "TauchTG" then -- Put everything you like :)
+      if not is_sudo(msg) then
+        return "For ArashTauch only !"
       else
         return bot_stats()
       end
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (TauchTG)",-- Put everything you like :)
+		"^[!/]([Tt]auchtg)"-- Put everything you like :)
     }, 
   run = run
 }
